a92c2d8eae0b65c6137e7c85f1c69a23376bb239
Paras Patil <117636855+paraspatil767@users.noreply.github.com>
2026-07-08 19:48:08 +0530

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int dept=0;
+    public int maxDepth(TreeNode root) {
+        return dsf(root);
+        // return dept;
+    }
+    public int dsf(TreeNode root)
+    {
+        if(root==null) return 0;
+        int left=dsf(root.left);
+        int right=dsf(root.right);
+        return Math.max(left,right)+1;
+    }
+}
