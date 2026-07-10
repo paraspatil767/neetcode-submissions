42997fe3858fe78bde3d31196ffc40cfe87075c8
Paras Patil <117636855+paraspatil767@users.noreply.github.com>
2026-07-10 13:02:12 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high)
+        {
+            int mid= low+(high-low)/2;
+
+            int num= nums[mid];
+
+            if(num==target)
+            {
+                return mid;
+            }
+            else if(num<target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
