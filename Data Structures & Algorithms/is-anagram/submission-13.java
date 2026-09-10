d31d493b2b63eecd77e2eeec3809c899695f2d5a
Paras Patil <117636855+paraspatil767@users.noreply.github.com>
2026-09-10 23:58:28 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length())
+        {
+            return false;
+        }
+
+        Map<Character,Integer> map= new HashMap<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            char sC=s.charAt(i);
+            char tC=t.charAt(i);
+            map.put(sC,map.getOrDefault(sC,0)+1);
+            map.put(tC,map.getOrDefault(tC,0)-1);
+
+        }
+        // for(int values: map.values())
+        // {
+        //     if(values!=0) return false;
+        // }
+
+        for(Map.Entry<Character,Integer> e:map.entrySet())
+        {
+            if(e.getValue()!=0) return false;
+        }
+        return true;
+    }
+}
